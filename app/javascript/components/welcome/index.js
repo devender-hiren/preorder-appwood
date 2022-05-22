3a88aa6572cc720a7fa2be043ead7919130cc544
devender-hiren <devhiren5@gmail.com>
2022-05-22 12:27:03 +0530

    added video popup button for introduction video

--- app/javascript/components/welcome/index.js
+++ app/javascript/components/welcome/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-import { Grid, Button } from "@mui/material";
+import { Grid, Button, Dialog, CardMedia } from "@mui/material";
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
 import Styled from "@emotion/styled";
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 function Index() {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return(
         <React.Fragment>
             <h1 className="page-title">👋 Welcome</h1>
@@ -27,7 +48,7 @@ function Index() {
                 </Grid>
             </Grid>
 
-            <Grid container spacing={3} sx={{ mt: 2, mb: 2 }}>
+            <Grid container spacing={3} sx={{ mt: 2, mb: 0 }}>
                 <Grid item xs={12} sm={6} md={3} sx={{ textAlign: { xs: "center", md: "left" } }}>
                     <Button variant="contained" color="primary">Open the Theme Editor</Button>
                 </Grid>
@@ -36,6 +57,36 @@ function Index() {
                     <Button variant="contained" color="secondary">Continue</Button>
                 </Grid>
             </Grid>
+
+            <Grid container spacing={3} sx={{ mt: 1, mb: 2 }}>
+                <Grid item xs={12} sx={{ textAlign: { xs: "center", md: "left" } }}>
+                    <Button variant="text" onClick={handleClickOpen} sx={{ textTransform: "none", fontWeight: 600, color: "#000000", fontSize: 18 }}>
+                        ( Check out our video )
+                    </Button>
+                </Grid>
+            </Grid>
+
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+                fullWidth={true}
+                maxWidth="sm"     
+            >
+                {/* <DialogTitle>{"Use Google's location service?"}</DialogTitle> */}
+                <DialogContent sx={{ padding: 0 }}>
+                    <CardMedia
+                        component="iframe"
+                        src="https://www.youtube.com/embed/EJr3WmbqzTY"
+                        height={320}
+                        sx={{ border: 0 }}
+                    />
+                    
+                </DialogContent>
+                
+            </Dialog>
         </React.Fragment>
     )
 }
